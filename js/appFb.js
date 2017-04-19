c63530a4c744e674a7d6de957dc857f72403fdd5
unknown <shoeabahmed4@gmail.com>
2017-04-19 16:04:20 +0600

    Real data fb posts

--- js/appFb.js
+++ js/appFb.js
@@ -2,22 +2,38 @@ var app = angular.module('myApp', ['ngSanitize']);
 app.controller('myCtrl', function($scope, $http,$sce) {
 	$scope.categories=[];
 	$scope.filtered = false;
-	$scope.tweets = [];
+	$scope.posts = [];
 
-  	$http.get("tweetsData.json")
+  	$http.get("harvard-fb-posts.json")
 	  	.then(function(response) {
 	  		console.log(response);
 	      // $scope.tweets = response.data;
-	      angular.forEach(response.data, function(data,key){
+	      angular.forEach(response.data.data, function(data,key){
 	      	//t = data.Text;
 			//data.text = data.Text.replace(/#(\w+)/g, '<a href="https://twitter.com/hashtag/$1">#$1</a>');
-			data.Text = data.Text.replace(/#(\S*)/g,'<a target="_blank" href="https://twitter.com/hashtag/$1?src=hash">#$1</a>');
+			//data.message = data.message.replace(/#(\S*)/g,'<a target="_blank" href="https://twitter.com/hashtag/$1?src=hash">#$1</a>');
+			
+			data.message = urlify(data.message);
+
 			//console.log(t);
-			$scope.tweets.push(data);
+			$scope.posts.push(data);
 	      });
 	      /*console.log(response.data.Entities)
 	      $scope.dataType = typeof(response.data);*/
+	      //$scope.posts = response.data.data;
+	      console.log($scope.posts);
 		});
+
+	function urlify(text) {
+	    var urlRegex = /(https?:\/\/[^\s]+)/g;
+	    return text.replace(urlRegex, function(url) {
+	        return '<a href="' + url + '">' + url + '</a>';
+	    })
+	    // or alternatively
+	    // return text.replace(urlRegex, '<a href="$1">$1</a>')
+	}
+
+	
 
 	$scope.filterTweet = function(){
 		if($scope.categoryFilter!='' || $scope.categoryFilter!=undefined)
